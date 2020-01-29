b36010ceccc2066621a28c7ef4245790a9268686
Ariets1910 <Ariets1910@gmail.com>
2020-01-29 14:50:34 +0200

    Css for every component

--- react-kabzda-1/src/Components/Header.jsx
+++ react-kabzda-1/src/Components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-    const Header = () => {
-        return (<header className='header'>
-            <img src="https://www.logoground.com/uploads/201526212015-10-262634559pepperspice.jpg" alt=""/>
-        </header>)
-    };
-    export default Header
+import "./Header.css"
+const Header = () => {
+    return (<header className='header'>
+        <img src="https://www.logoground.com/uploads/201526212015-10-262634559pepperspice.jpg" alt=""/>
+    </header>)
+};
+export default Header
