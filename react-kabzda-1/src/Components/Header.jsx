e1703ae86f53623d5d24c46c312123ac9be60061
Михайло Сюсько <Ariets1910@gmail.com>
2020-01-29 12:54:41 +0200

    all files

---
+++ react-kabzda-1/src/Components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+    const Header = () => {
+        return (<header className='header'>
+            <img src="https://www.logoground.com/uploads/201526212015-10-262634559pepperspice.jpg" alt=""/>
+        </header>)
+    };
+    export default Header
---
+++ react-kabzda-1/src/Components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <img src="https://kogdakotika.net/media/post_images/title_page_m_ChtRYfI.jpg"/>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>)
+
+};
+
+export default Profile
